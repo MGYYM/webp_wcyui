88aea1e8d8853f5097cb288716f70098d0ff41f9
yuym <1qaz@WSX0927>
2021-01-14 15:21:44 +0800

    添加事件

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 //挂载到全局
-import NetWork  from './common/netWork'
-import appConfig  from './common/appConfig'
+import NetWork  from './common/netWork/index'
+import appConfig  from './common/appConfig/index'
+import {$on,$emit,$remove}  from './common/event/index'
 import Base from 'base'
 class Whcy extends Base{
     constructor(baseUrl,app) {
        console.log(app);
        super(app);
        this.request = new NetWork(baseUrl,app).request;
+       this.$on = $on;
+       this.$emit = $emit;
+       this.$remove = $remove;
     }
 }
 export { Whcy,appConfig }
